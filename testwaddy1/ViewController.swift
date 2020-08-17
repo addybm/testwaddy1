dad93cdfcac54e4345f7826efa2ad8a74ef34ec5
ericazhang17 <zzhang4@wellesley.edu>
2020-08-17 19:37:16 -0400

    Initial Commit

---
+++ testwaddy1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testwaddy1
+//
+//  Created by Erica Zhang on 8/17/20.
+//  Copyright © 2020 Erica . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
